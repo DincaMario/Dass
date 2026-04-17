22225c2539ffe3c0d5e8f535ab9f81903f4b2722
Dinca Mario <mario.dinca5@yahoo.com>
2026-04-17 16:14:45 +0300

    adaugare pagini login si register

---
+++ static/js/auth.js
@@ -0,0 +1,39 @@
+async function Register(){
+    var email = document.getElementById('email').value
+    var password = document.getElementById('password').value;
+    var confirmPassword = document.getElementById('confirmPassword').value
+    var role = document.getElementById('role').value
+
+    var res = await fetch('/register', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json'},
+        body: JSON.stringify({email: email, password: password, role: role, confirmPassword: confirmPassword})
+    })
+
+    var data = await res.json();
+    if( res.ok){
+        window.location.href = '/login'
+    }else{
+        console.log(data.error)
+    }
+}
+
+async function Login(){
+    var email = document.getElementById('email').value
+    var password = document.getElementById('password').value;
+
+        var res = await fetch('/login', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json'},
+        body: JSON.stringify({email: email, password: password})
+    })
+
+    var data = await res.json();
+
+        var data = await res.json();
+    if( res.ok){
+        window.location.href = '/dashboard'
+    }else{
+        console.log(data.error)
+    }
+}
